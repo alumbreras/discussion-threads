8c8b2163317e20a06d4cee2956022be8c158e693
Alberto Lumbreras <alberto.lumbreras@gmail.com>
2016-06-14 20:13:55 +0200

    in threads generated by Lumbreras, user frequency distribution follows distribution in real data

--- R/thread_generators.R
+++ R/thread_generators.R
@@ -11,25 +11,25 @@ gen.thread.Gomez2011 <- function(n=100, alpha.root=1, alpha.c = 1, beta.root = 1
   #    alpha:  preferential attachement exponent
   #    beta: root bias
   g <- graph.empty(n=1)
-  
+
   # First post has no choice
   g <- add_vertices(g, 1)
   g <- add_edges(g, c(2,1))
-  
+
   for (i in 3:n){
     alphas <- c(alpha.root, rep(alpha.c, i-2))
     betas <- c(beta.root, rep(1, i-2))
     popularities <- 1 + degree(g, mode="in")
-    
+
     # Probability of choosing every node (only one is chosen)
     probs <- (betas*popularities)^alphas
     probs <- probs/sum(probs)
     j <- sample(1:length(probs), 1, prob=probs)
-    
+
     # Add new vertex attached to the chosen node
     g <- add_vertices(g, 1)
     g <- add_edges(g, c(i,j))
-  } 
+  }
   g
 }
 
@@ -41,21 +41,21 @@ gen.thread.Gomez2013 <- function(n=100, alpha=1, beta = 1, tau=0.75){
   #    alpha:  preferential attachement exponent
   #    beta: root bias
   g <- graph.empty(n=1)
-  
+
   # First post has no choice
   g <- add_vertices(g, 1)
   g <- add_edges(g, c(2,1))
-  
+
   for (i in 3:n){
     betas <- c(beta, rep(0, i-2))
     lags <- (i-1):1
     popularities <- 1 + degree(g, mode="in") # even root starts with degree 1
-    
+
     # Probability of choosing every node (only one is chosen)
     probs <- alpha*popularities + betas + tau^lags
     probs <- probs/sum(probs)
     j <- sample(1:length(probs), 1, prob=probs)
-    
+
     # Add new vertex attached to the chosen node
     g <- add_vertices(g, 1)
     g <- add_edges(g, c(i,j))
@@ -64,7 +64,9 @@ gen.thread.Gomez2013 <- function(n=100, alpha=1, beta = 1, tau=0.75){
   g
 }
 
-gen.thread.Lumbreras2016 <- function(n=100, z=c(1,2,3), alphas = c(1,2,3), betas = c(1,2,3), taus = c(0.25, 0.5, 0.75)){
+gen.thread.Lumbreras2016 <- function(n=100, z=c(1,2,3), user.freqs=rep(1,length(z)),
+                                     alphas = c(1,2,3), betas = c(1,2,3), taus = c(0.25, 0.5, 0.75),
+                                     user.samples){
   # Generate threads like in Gomez 2013 but from a mixture model where users
   # in one component have different parameters (alpha, beta, tau)  than users in the
   # other component
@@ -75,36 +77,36 @@ gen.thread.Lumbreras2016 <- function(n=100, z=c(1,2,3), alphas = c(1,2,3), betas
   #    beta: root bias
   nusers <- length(z)
   g <- graph.empty(n=1)
-  
+
   # First post has no choice
   g <- add_vertices(g, 1)
   g <- add_edges(g, c(2,1))
-  
+
   # the user of the two first posts is irrelevant for us
-  V(g)$user <- 1 
-  
+  V(g)$user <- 1
+
   for (i in 3:n){
-    # choose random user
-    u <- sample(nusers, 1)
+    # choose random user according to distribution in real data
+    u <- sample(user.samples, 1)
     cluster <- z[u]
     alpha <- alphas[cluster]
     beta <- betas[cluster]
     tau <- taus[cluster]
-    
+
     bs <- c(beta, rep(0, i-2))
     lags <- (i-1):1
     popularities <- 1 + degree(g, mode="in") # even root starts with degree 1
-    
+
     # Probability of choosing every node (only one is chosen)
     probs <- alpha*popularities + bs + tau^lags
     probs <- probs/sum(probs)
-    
+
     j <- sample(1:length(probs), 1, prob=probs)
-    
+
     # Add new vertex attached to the chosen node
     g <- add_vertices(g, 1, attr=list('user'= u))
     g <- add_edges(g, c(i,j))
-  } 
+  }
   g
 }
 
@@ -113,7 +115,7 @@ tree.to.data <- function(g, thread=0){
   # and columns "degree of parent", "is_parent_root", "lag to parent", and "t"
   # With the model parameters, the three first columns are used to compute the numerator the likelihood
   # and 't' to compute the denominator of the likelihood
-  
+
   parents <- get.edgelist(g, names=FALSE)[,2] # parents vector without the first two posts
   authors <- V(g)$user[-1] # remove first post
   popularities <- c(1,sapply(2:length(parents), function(t) 1 + sum(parents[1:(t-1)]==parents[t])))
@@ -125,7 +127,7 @@ tree.to.data <- function(g, thread=0){
                      t = 1:(length(parents)),
                      parent = parents,
                      popularity = popularities)
-  
+
   data <- mutate(data, lag=t-parent+1)
   data
 }
@@ -135,17 +137,17 @@ tree.to.data <- function(g, thread=0){
 #######################################
 if(FALSE){
   n <- 100
-  
+
   alpha.root <- 0.734
   alpha.c <- 0.683
   beta.root <- 1.302
   trees <- replicate(n,  gen.thread.Gomez2011(n=20, alpha.root=alpha.root, alpha.c=alpha.c, beta.root=beta.root), simplify = FALSE)
-  
+
   alpha <- 1
   beta <- 0.68
   tau <- 0.75
   trees <- replicate(100, gen.thread.Gomez2013(n=100, alpha=alpha, beta=beta, tau=tau), simplify = FALSE)
-  
+
   # Synthetic trees for Lumbreras 2016
   set.seed(1)
   alphas <- c(1,2,3)
@@ -153,31 +155,31 @@ if(FALSE){
   taus <- c(0.25, 0.5, 0.75)
   z <- c(1,2,3,1,2,3,1,2,3,1,2,3)
   trees <- replicate(1000,  gen.thread.Lumbreras2016(n=100, z=z, alphas=alphas, betas=betas, taus=taus), simplify = FALSE)
-  
+
   # Very simple synthetic trees for Lumbreras 2016
   set.seed(1)
   alphas <- c(0.1,0.5)
   betas <- c(0.1,0.5)
   taus <- c(0.1, 0.5)
   z <- c(1,2)
-  
+
   set.seed(1)
   alphas <- c(0.1, 0.5, 1)
   betas <- c(1, 5, 10)
   taus <- c(0.1, 0.5, 0.99)
   z <- do.call(c, lapply(1:length(alphas), function(x) rep(x,10000)))
-  
+
   # the parallel way
   library(parallel)
-  cl <- makeCluster(detectCores()-2)  
+  cl <- makeCluster(detectCores()-2)
   clusterEvalQ(cl, {library(igraph); source('thread_generator.r')})
   clusterExport(cl, c("alphas", "betas", "taus", "z"))
   trees <- parLapply(cl, 1:20000, function(i) gen.thread.Lumbreras2016(n=25, z=z, alphas=alphas, betas=betas, taus=taus) )
   stopCluster(cl)
-  
+
   save(trees, file='trees.Rda')
-  
-  
+
+
   # Create dataframe from a set of trees
   ############################################################
   # Prepare all the information needed into a nice dataframe
@@ -187,22 +189,22 @@ if(FALSE){
   clusterEvalQ(cl, {library(igraph); library(dplyr)})
   data.parlapply <- parLapply(cl, trees, tree.to.data)
   stopCluster(cl)
-  
+
   # join results adding thread id
   for (i in 1:length(data.parlapply)){
     data.parlapply[[i]]$thread <- i
   }
   data.parlapply <- rbindlist(data.parlapply)
-  df.trees <- data.frame(thread = data.parlapply$thread, 
+  df.trees <- data.frame(thread = data.parlapply$thread,
                          user = data.parlapply$user,
                          post = data.parlapply$post,
                          t = data.parlapply$t,
                          parent = data.parlapply$parent,
                          popularity = data.parlapply$popularity,
                          lag = data.parlapply$lag)
-  
+
   save(df.trees, file='df.trees.Rda')
-  
+
 }
 
 # Generate and plot some synthetic tree
@@ -210,36 +212,36 @@ if(FALSE){
 TEST <- FALSE
 if(TEST){
   # Values reported in Gomze 2010 (Table 2)
-  # Slashdot: 
+  # Slashdot:
   alpha.root <- 0.734
   alpha.c <- 0.683
   beta.root <- 1.302
-  
+
   # Barrapunto
   alpha.root <- 0.665
   alpha.c <- -0.116
   beta.root <- 0.781
-  
+
   # Meneame
   alpha.root <- 0.856
   alpha.c <- 0.196
   beta.root <- 1.588
-  
+
   # Wikipedia
   alpha.root <- 0.884
   alpha.c <- -1.684
   beta.root <- 0.794
-  
+
   trees <- replicate(4,  gen.thread.Gomez2011(n=100, alpha.root=alpha.root, alpha.c=alpha.c, beta.root=beta.root), simplify = FALSE)
-  
-  
+
+
   alpha <- 1
   beta <- 0.68
   tau <- 0.75
   trees <- replicate(4,  gen.thread.Gomez2013(n=100, alpha=alpha, beta=beta, tau=tau), simplify = FALSE)
-  
+
   trees <- replicate(4,  gen.thread.Lumbreras2016(n=100), simplify = FALSE)
-  
+
   par(mfrow=c(2,2))
   for (i in 1:length(trees)){
     g <- trees[[i]]
