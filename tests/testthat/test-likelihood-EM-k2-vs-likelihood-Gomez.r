ea998fe1611eefdd7dfb5d4f39912b6516822f4a
Alberto Lumbreras <alberto.lumbreras@gmail.com>
2016-06-14 15:12:50 +0200

    fixing tests after restructure of directories

--- tests/testthat/test-likelihood-EM-k2-vs-likelihood-Gomez.r
+++ tests/testthat/test-likelihood-EM-k2-vs-likelihood-Gomez.r
@@ -2,14 +2,10 @@ context("likelihood-EM")
 # Test that the solution of two components with the same parameters
 # has the same likelihood than one component with that parameters
 
-source('R/datasets.R')
-source('R/likelihood.R')
-source('R/extract_from_db.R')
-source('R/estimators.R')
-
 # Load dataframe representation of trees
-load('data/trees.gof.rda')
-load('data/df.trees.gof.rda')
+data.dir <- file.path(system.file("data", package="threads"))
+load(paste0(data.dir, '/trees.gof.rda'))
+load(paste0(data.dir, '/df.trees.gof.rda'))
 
 # Subset for testing purposes
 trees <- trees[1:10]
@@ -22,8 +18,8 @@ df.trees$userint <- match(df.trees$user, users)
 U <- length(unique(df.trees$userint))
 
 params.gomez <- c(0.5, 0.5, 0.5)
-params.lumbreras <- list(alphas=rep(params.gomez[1],2), 
-                         betas=rep(params.gomez[2],2), 
+params.lumbreras <- list(alphas=rep(params.gomez[1],2),
+                         betas=rep(params.gomez[2],2),
                          taus=rep(params.gomez[3],2))
 responsabilities <- matrix(c(0.5,0.5), nrow=U, ncol=2)
 pis <- c(0.5,0.5)
