25c0b13894b98dac7bbed7dd82f6a1f986f2daff
Alberto Lumbreras <alberto.lumbreras@gmail.com>
2016-06-19 19:34:13 +0200

    starting with recommendations

--- analysis/main.R
+++ analysis/main.R
@@ -7,10 +7,10 @@
 trees <- load_trees(forum="reddit", subforum="podemos")
 df.trees <- trees_to_dataframe(trees)
 
-save(trees, file='data/trees.podemos.rda')
-save(df.trees, file='data/df.trees.podemos.rda')
-#load('data/trees.gof.rda')
-#load('data/df.trees.gof.rda')
+save(trees, file='../data/trees.podemos.rda')
+save(df.trees, file='../data/df.trees.podemos.rda')
+#load('../data/trees.podemos.rda')
+#load('../data/df.trees.podemos.rda')
 
 # In case users are string names, we give each user a unique integer id
 # besides, ids are given according to the user frequency (though not necessary)
@@ -39,8 +39,8 @@ title('\n Likelihood')
 
 save(params.gomez, file='data/params.gomez.podemos.rda')
 save(params.lumbreras, file='data/params.lumbreras.podemos.rda')
-#load('data/params.gomez.rda')
-#load('data/params.lumbreras.rda')
+#load('../data/params.gomez.rda')
+#load('../data/params.lumbreras.rda')
 
 # Compare likelihood Gomez2013 and Lumbreras2016 over the trees
 likelihood.gomez <- likelihood_Gomez2013(df.trees, params.gomez)
@@ -69,3 +69,10 @@ compare_trees(trees, trees.lumbreras, trees.gomez)
 
 # Compare how good are both models to make posts recommendations
 # (if we recommend the most likely parent)
+# isert user class in the tree 
+#TODO: this should be done once on top of the script
+users <- names(sort(table(df.trees$user), decreasing = TRUE))
+
+trees_ <- lapply(trees, function(t) {V(t)$userint <- match(V(t)$user, users);t})
+
+compare_link_prediction(trees_, param.lumbreras, params.gomez)
